706d3c916c49e4f97d8f78f4395a8f497abdd784
youssef <youssef.abidi@polytechnicien.tn>
2025-12-10 09:18:06 +0100

    Ajout de la fonctionnalite d'ajout de taches.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("To-do App Initialisee.");
+functionajouterTache() {
+    const tacheInput = document.getElementById("taSKInput");
+    const texte =InputDeviceInfo.value.trim();
+    if (texte !== "") {
+        const li = document.createElement("li");
+        li.textContent = texte;
+        document.getElementById("taskList").appendChild(li);
+        tacheInput.value = "";
+    }
+}
+const boutonAjout = document.getElementById("addTaskBtn");
+boutonAjout.addEventListener("click", ajouterTache);
